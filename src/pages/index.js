d26e125dfad05e7e72d40f617c1d276ac71e206c
leachjustin18 <leachjustin18@yahoo.com>
2018-11-10 16:50:52 -0600

    Change max width of image.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <figure style={{ maxWidth: '600px', marginBottom: '1.45rem' }}>
+    <figure style={{ maxWidth: '450px', marginBottom: '1.45rem' }}>
       <Image />
     </figure>
     <Link to="/page-2/">Go to page 2</Link>
