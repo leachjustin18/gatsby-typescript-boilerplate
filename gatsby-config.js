66bb76c0665f90d20f2006488e6af002d6dc7a95
leachjustin18 <leachjustin18@yahoo.com>
2018-11-10 16:57:39 -0600

    Added favicon pkg.
    Renamed favion img.
    
    Placed favicon at root.

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,9 +22,10 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: './src/favicon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
+    'gatsby-plugin-favicon'
   ],
 }
