13ccbe08417b80f442ef027a0f6b56bdf01b1b96
leachjustin18 <leachjustin18@yahoo.com>
2018-11-11 21:06:56 -0600

    Added root scss with modified normilize
    Added layout.scss.  Removed layout.css

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
 import Header from '../components/Header';
-import './layout.css';
+import './layout.scss';
 
 interface LayoutProps {
   className?: string;
