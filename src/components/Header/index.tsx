9a7ce64e1ceb73362bbf31e098ba5e78f291fbf7
leachjustin18 <leachjustin18@yahoo.com>
2018-11-12 21:00:57 -0600

    Moved header styles into scss file
    Deleted extra div.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import './Header.scss';
 
 interface HeaderProps {
   siteTitle?: string;
@@ -10,32 +11,13 @@ class Header extends React.PureComponent<HeaderProps, {}> {
     const { siteTitle } = this.props;
 
     return (
-      <div
-        style={{
-          background: 'rebeccapurple',
-          marginBottom: '1.45rem',
-        }}
-      >
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '1.45rem 1.0875rem',
-          }}
-        >
-          <h1 style={{ margin: 0 }}>
-            <Link
-              to="/"
-              style={{
-                color: 'white',
-                textDecoration: 'none',
-              }}
-            >
-              {siteTitle}
-            </Link>
-          </h1>
-        </div>
-      </div>
+      <header className="header">
+        <h1>
+          <Link to="/" className="header__link">
+            {siteTitle}
+          </Link>
+        </h1>
+      </header>
     );
   }
 }
