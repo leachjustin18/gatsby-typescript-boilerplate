d0755337e3a2e10d6332818f75f94b5a98a33585
leachjustin18 <leachjustin18@yahoo.com>
2018-11-12 21:04:53 -0600

    Added styles for index page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'gatsby';
-
 import Layout from '../layout';
 import Image from '../components/Image';
+import './indexPage.scss';
 
 class IndexPage extends React.PureComponent<{}, {}> {
   public render() {
@@ -11,7 +11,7 @@ class IndexPage extends React.PureComponent<{}, {}> {
         <h1>Hi people</h1>
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
-        <figure style={{ maxWidth: '450px', marginBottom: '1.45rem' }}>
+        <figure className="indexPage__imgContainer">
           <Image />
         </figure>
         <Link to="/page-2/">Go to page 2</Link>
